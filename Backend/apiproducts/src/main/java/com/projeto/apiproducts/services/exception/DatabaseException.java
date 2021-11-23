4185da859a98fc976a5e1f5a526c37c2c05114a6
Arthur Scarpati <arthur_159@hotmail.com>
2021-11-23 10:47:41 -0300

    Product Delete and Update

---
+++ Backend/apiproducts/src/main/java/com/projeto/apiproducts/services/exception/DatabaseException.java
@@ -0,0 +1,10 @@
+package com.projeto.apiproducts.services.exception;
+
+public class DatabaseException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+}
